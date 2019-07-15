1ce104beddea1eebc1d5a18b51bbe511dbc9beba
jiongliuzhou <798676143@qq.com>
2019-07-15 15:58:46 +0800

    认证中心服务端

--- authentication/src/main/java/com/lz/authentication/AuthenticationApplication.java
+++ authentication/src/main/java/com/lz/authentication/AuthenticationApplication.java
@@ -2,8 +2,10 @@ package com.lz.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.lz")
 public class AuthenticationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationApplication.class, args);
