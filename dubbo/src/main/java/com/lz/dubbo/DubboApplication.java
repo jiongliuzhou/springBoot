a8921481648550d422f824da5b40415b163d74e8
jiongliuzhou <798676143@qq.com>
2019-07-15 09:39:40 +0800

    dubbo服务提供者

--- dubbo/src/main/java/com/lz/dubbo/DubboApplication.java
+++ dubbo/src/main/java/com/lz/dubbo/DubboApplication.java
@@ -1,9 +1,11 @@
 package com.lz.dubbo;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo(scanBasePackages ="com.zl.dubbo")
 public class DubboApplication {
 
 	public static void main(String[] args) {
