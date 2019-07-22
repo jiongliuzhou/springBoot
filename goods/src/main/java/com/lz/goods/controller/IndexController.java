a6824261e1b30681e2ee847cede4561e38bcc105
jiongliuzhou <798676143@qq.com>
2019-07-22 17:48:15 +0800

    商品管理系统-增加前端页面

---
+++ goods/src/main/java/com/lz/goods/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.lz.goods.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author lz
+ * @create 2019/7/22
+ * @since 1.0.0
+ */
+@Controller
+public class IndexController {
+
+    /**
+     * 跳转到首页
+     * @return
+     */
+    @RequestMapping("/index")
+    public String index(){
+        return "list";
+    }
+}
