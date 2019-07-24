08eb0f037881c8e4bddd8c5776e214ff766aa364
jiongliuzhou <798676143@qq.com>
2019-07-24 11:30:50 +0800

    认证服务系统-登录页面引入bootstrap.css文件导致登录成功后页面无法跳转问题，重命名bootstrap.css为botstrap.css

--- authentication/src/main/java/com/lz/authentication/controller/LoginController.java
+++ authentication/src/main/java/com/lz/authentication/controller/LoginController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,7 +21,7 @@ import java.io.IOException;
 @Slf4j
 public class LoginController {
 
-    @GetMapping("/login")
+    @RequestMapping("/login")
     public String login() {
         return "login";
     }
