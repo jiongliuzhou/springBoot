d672b1c1534d9f5ef6665254ff0024b5bfab1b2a
liuzhou <liuzhou@liuzhou.com>
2019-07-28 13:04:39 +0800

    认证服务器-增加前端页面

--- authentication/src/main/java/com/lz/authentication/controller/UserController.java
+++ authentication/src/main/java/com/lz/authentication/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.lz.authentication.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.lz.api.service.SysUserApiService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -15,6 +19,30 @@ public class UserController {
     @GetMapping("/user/me")
     public Principal user(Principal principal) {
         return principal;
+    }
+
+    @Reference
+    private SysUserApiService sysUserApiService;
+    /**
+     * 获取用户名
+     * @param principal
+     * @return
+     */
+    @RequestMapping("/userController/userName")
+    @ResponseBody
+    public Object userName(Principal principal) {
+        return principal.getName();
+    }
+
+    /**
+     * 获取当前用户信息
+     * @param principal
+     * @return
+     */
+    @RequestMapping("/userController/getUserInfo")
+    @ResponseBody
+    public Object getUserInfo(Principal principal){
+        return sysUserApiService.getSysUserInfo(principal.getName());
     }
 }
 
